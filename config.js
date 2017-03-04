6e0a0aebf041ce68c1d3a300cc0df7bdbf2ee9b0
karenda <pkurilin@gmail.com>
2017-03-04 22:08:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,clipboard,button,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,copyformatting,div,resize,toolbar,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,undo,wsc,notification,wordcount,youtube,videodetector,uicolor,texttransform,textsignature,tabbedimagebrowser,tableresize,tabletoolstoolbar,stylesheetparser,sourcedialog,lineutils,widgetselection,widget,soundPlayer,staticspace,stat,stylesheetparser-fixed,syntaxhighlight,slideshow,sketchfab,SimpleLink,simple-image-browser,SimpleImage,html5audio,simplebutton,simple-ruler,smallerselection,SPImage,notificationaggregator,embedbase,embedsemantic,placeholder_select,placeholder,niftyimages,pastefromexcel,pastecode,osem_googlemaps,page2images,numericinput,tliyoutube2,embed,maxheight,markdown,lineheight,letterspacing,lightbox,liveedit,basewidget,layoutmanager,textselection,insertpre,symbol,inlinesave,inlinecancel,closebtn,imgur,iframedialog,html5video,htmlbuttons,Audio,imgupload,imgbrowse,imagebrowser,imagepaste,imageresize,imagerotate,imageuploader,image2,imageresponsive,googledocs,format_buttons,ckeditor-gwf-plugin,googlethisterm,html5validation,label,floating-tools,fixed,filetools,fastimage,emojione,easykeymap,dropdownmenumanager,extraformattributes,divarea,docprops,docfont,cssanim,custimage,enhancedcolorbuttton,eqneditor,FMathEditor,devtools,confighelper,ccmsconfighelper,codemirror,pre,niftytimers,codeTag,xml,ajax,codesnippet,codesnippetgeshi,pbckcode,crossreference,cleanuploader,footnotes,ckawesome,ckwebspeech,chart,glyphicons,bbcode,btbutton,base64image,bt_table,btquicktable,backgrounds,bootstrapTabs,bootstrapVisibility,balloonpanel,bgimage,autosave,autocorrect,autolink,autoembed,autogrow,brclear,allowsave,ccmsacdc,resizewithwindow,selectallcontextmenu,spoiler,Text2Speech,token,zoom';
+	config.skin = 'moono-dark';
+	// %REMOVE_END%
+
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
